08430603523fb544506c643afd58b14ee3efa243
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2024-08-01 12:06:01 -0500

    consolidate and rename animator.js and algoAnimation.js to animation.js

--- animation.js
+++ animation.js
@@ -1,3 +1,14 @@
+class AlgoAnimation {
+	constructor(animation = { type: "", value: null, index: null, indexes: [], elements: [], sleepOverride: null }) {
+		this.type = animation.type;
+		this.value = animation.value;
+		this.index = animation.index;
+		this.indexes = animation.indexes;
+		this.elements = animation.elements;
+		this.sleepOverride = animation.sleepOverride;
+	}
+}
+
 class Animator {
 	/**
 	 *
